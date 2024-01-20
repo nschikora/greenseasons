3d5b0ff4393ee5ca1faf544634b5a136dadfaf0a
Nils Schikora <nschikora@icloud.com>
2024-01-20 18:29:57 +0100

    initial commit

---
+++ components/legend.tsx
@@ -0,0 +1,18 @@
+export function Legend() {
+  return (
+    <legend className="flex items-start flex-col justify-center gap-1 p-4">
+      <div className="flex items-center justify-center">
+        <div className="w-4 h-4 bg-emerald-400"></div>
+        <p className="ml-1">Saisonal</p>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="w-4 h-4 bg-sky-400"></div>
+        <p className="ml-1">Eingelagert</p>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="w-4 h-4 bg-slate-400"></div>
+        <p className="ml-1">Importiert</p>
+      </div>
+    </legend>
+  );
+}
---
+++ components/seasonal-calendar.tsx
@@ -0,0 +1,70 @@
+import fruits from "./fruits.json";
+import { Legend } from "./legend";
+import salad from "./salad.json";
+import vegetables from "./vegetables.json";
+
+// Each the elements imported from the JSON files are arrays of objects of the following shape:
+// {
+//     "vegetable": "Radicchio",
+//     "available": [8, 9, 10, 11],
+//     "storage": [1, 2, 12],
+//     "notAvailable": [3, 4, 5, 6, 7]
+// },
+
+// merge the arrays and order them by when they are available
+const seasonalCalendar = [...fruits, ...salad, ...vegetables].sort(
+  (a, b) => a.available[0] - b.available[0]
+);
+
+// render a table with the fruits etc in y and months in x. Hightlight when they are available, storage or not available
+export function SeasonalCalendar() {
+  return (
+    <>
+      <table className="w-full">
+        <thead>
+          <tr>
+            <th className="bg-white absolute top-0 left-0 z-10 px-1 text-white">
+              Frühlingszwiebeln
+            </th>
+            {Array.from(Array(12).keys()).map((month) => (
+              <th key={month} className="px-8 bg-white">
+                {
+                  // get localised monht name
+                  new Date(2021, month).toLocaleString("default", {
+                    month: "short",
+                  })
+                }
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {seasonalCalendar.map((item) => (
+            <tr key={item.vegetable} className="border-b border-b-slate-200">
+              <td className="px-1 bg-white">{item.vegetable}</td>
+              {Array.from(Array(12).keys()).map((month) => (
+                <td
+                  key={month}
+                  className={
+                    item.available.includes(month + 1)
+                      ? "bg-emerald-400"
+                      : item.storage.includes(month + 1)
+                      ? "bg-sky-400"
+                      : item.notAvailable.includes(month + 1)
+                      ? "bg-slate-400"
+                      : ""
+                  }
+                ></td>
+              ))}
+            </tr>
+          ))}
+          <tr>
+            <td>
+              <Legend />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
