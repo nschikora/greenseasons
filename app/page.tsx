b6a2ea86c8b8695318bcf36016310f0f18c9e6fc
Nils Schikora <nschikora@icloud.com>
2024-01-20 18:56:31 +0100

    use svh instead of vh

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { SeasonalCalendar } from "@/components/seasonal-calendar";
 
 export default function Home() {
   return (
-    <main className="flex lg:justify-center overflow-auto h-screen">
+    <main className="flex lg:justify-center overflow-auto h-[100svh]">
       <SeasonalCalendar />
     </main>
   );
